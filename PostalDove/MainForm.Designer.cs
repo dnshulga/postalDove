0b3c01f74276fbca26668abcd77e4ffb3b9039dc
dnshulga <dnshulga@ya.ru>
2016-02-12 21:38:55 +0200

    mainform was designed

--- PostalDove/MainForm.Designer.cs
+++ PostalDove/MainForm.Designer.cs
@@ -28,20 +28,143 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.OptionsStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.AboutStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.subjTxtBox = new System.Windows.Forms.TextBox();
+            this.bodyTxtBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.htmlCheckBox = new System.Windows.Forms.CheckBox();
+            this.тестоваяОтправкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OptionsStrip,
+            this.тестоваяОтправкаToolStripMenuItem,
+            this.AboutStrip});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(343, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // OptionsStrip
+            // 
+            this.OptionsStrip.Name = "OptionsStrip";
+            this.OptionsStrip.Size = new System.Drawing.Size(79, 20);
+            this.OptionsStrip.Text = "Настройки";
+            // 
+            // AboutStrip
+            // 
+            this.AboutStrip.Name = "AboutStrip";
+            this.AboutStrip.Size = new System.Drawing.Size(94, 20);
+            this.AboutStrip.Text = "О программе";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Тема письма:";
+            // 
+            // subjTxtBox
+            // 
+            this.subjTxtBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.subjTxtBox.Location = new System.Drawing.Point(15, 61);
+            this.subjTxtBox.Name = "subjTxtBox";
+            this.subjTxtBox.Size = new System.Drawing.Size(310, 20);
+            this.subjTxtBox.TabIndex = 2;
+            // 
+            // bodyTxtBox
+            // 
+            this.bodyTxtBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bodyTxtBox.Location = new System.Drawing.Point(15, 114);
+            this.bodyTxtBox.Multiline = true;
+            this.bodyTxtBox.Name = "bodyTxtBox";
+            this.bodyTxtBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.bodyTxtBox.Size = new System.Drawing.Size(310, 193);
+            this.bodyTxtBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Текст письма:";
+            // 
+            // htmlCheckBox
+            // 
+            this.htmlCheckBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.htmlCheckBox.AutoSize = true;
+            this.htmlCheckBox.Location = new System.Drawing.Point(15, 324);
+            this.htmlCheckBox.Name = "htmlCheckBox";
+            this.htmlCheckBox.Size = new System.Drawing.Size(135, 17);
+            this.htmlCheckBox.TabIndex = 5;
+            this.htmlCheckBox.Text = "Текст в формате html";
+            this.htmlCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // тестоваяОтправкаToolStripMenuItem
+            // 
+            this.тестоваяОтправкаToolStripMenuItem.Name = "тестоваяОтправкаToolStripMenuItem";
+            this.тестоваяОтправкаToolStripMenuItem.Size = new System.Drawing.Size(121, 20);
+            this.тестоваяОтправкаToolStripMenuItem.Text = "Тестовая отправка";
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.sendBtn.Location = new System.Drawing.Point(237, 350);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(88, 33);
+            this.sendBtn.TabIndex = 6;
+            this.sendBtn.Text = "Отправить";
+            this.sendBtn.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(536, 321);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(343, 395);
+            this.Controls.Add(this.sendBtn);
+            this.Controls.Add(this.htmlCheckBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.bodyTxtBox);
+            this.Controls.Add(this.subjTxtBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Postal Dove";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem OptionsStrip;
+        private System.Windows.Forms.ToolStripMenuItem AboutStrip;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox subjTxtBox;
+        private System.Windows.Forms.TextBox bodyTxtBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripMenuItem тестоваяОтправкаToolStripMenuItem;
+        private System.Windows.Forms.CheckBox htmlCheckBox;
+        private System.Windows.Forms.Button sendBtn;
     }
 }
 
